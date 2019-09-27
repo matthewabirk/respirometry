7dcb540df37d7444ad14eda0413983219f6b6b1c
Matthew A. Birk <matthewabirk@gmail.com>
2019-09-27 07:13:00 -0400

    v 1.0.1

---
+++ respirometry_1.0.1/R/respirometry.R
@@ -0,0 +1,10 @@
+#' Tools for Conducting Respirometry Experiments
+#'
+#' Provides tools to enable the researcher to more precisely conduct respirometry experiments. Strong emphasis is on aquatic respirometry. Tools focus on helping the researcher setup and conduct experiments. Analysis of the resulting data is not a focus since analyses are often specific to a particular setup, and thus are better created by the researcher individually. This package provides tools for intermittent, flow-through, and closed respirometry techniques.
+#'
+#' @author Matthew A. Birk, \email{matthewabirk@@gmail.com}
+#' @docType package
+#' @name respirometry
+#' @encoding UTF-8
+
+NULL
